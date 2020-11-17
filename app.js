f93aa25b1e22f75b1552bededad5aa959d8c716e
sethhoward21 <71048691+sethhoward21@users.noreply.github.com>
2020-11-16 21:21:01 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,146 @@
+// // Maximum Number
+
+// // index = 1,  2, 3, 4, 5
+// let nums = [3, 25, 1, 9]
+// // length = 5
+
+// let maxNums = nums[0]
+// for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] > maxNums) {
+//     maxNums = nums[i]
+//     }
+// }
+
+// console.log(maxNums)
+
+// //Minimum Number
+
+// // index =  0,  1, 2, 3, 4
+// let nums = [29, 45, 102, 7, 19]
+// // length = 5
+
+// let minNums = nums[0]
+// for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] < minNums) {
+//         minNums = nums [i]
+//     }
+// }
+// console.log(minNums)
+
+
+// //Odd Numbers
+
+// let nums = [1, 2, 3, 4, 5, 6, 7]
+
+
+// let oddNums = 0
+// for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] % 2 == 1) {
+//         oddNums = oddNums + 1
+//     }
+    
+// }
+// console.log(oddNums)
+
+
+
+// //Zoo Animals
+
+// let monkey = 1
+// let zebra = 2
+// let hippo = 3
+// let penguin = 4
+
+// let anims = [monkey, zebra, hippo, penguin]
+
+
+// //favorite is penguin
+
+// let fav = 0
+
+// for (let i = 0; i < anims.length; i++) {
+//     if (anims[i] == 4) {
+//         fav = fav + 5
+//     }
+// }
+
+// console.log(fav)
+
+// // 5 in log means present
+// // 0 in log means absent
+
+
+
+
+// //Fahrenheit to Celsius
+
+// let nums = [54, 88, 25, 99, 76]
+
+// let cels = nums.map(num => {
+//     return (num - 32) * (5/9)
+// })
+
+// console.log(cels)
+
+
+
+
+// //First number divisible by 5
+
+// let nums = [3, 7, 9, 10, 12, 15]
+
+// let firstFive = nums[0]
+
+
+// for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] % 5 == 0) {
+//         if (i)
+//         firstFive = i
+//     }
+// }
+
+// console.log(firstFive)
+
+
+
+
+// // Vowel Count
+
+// let word = "mountain vista"
+// let vowCount = 0
+
+// for (let i = 0; i < word.length; i++) {
+//     if (word[i] == "a") {
+//     vowCount = vowCount + 1
+//     }
+//     if (word[i] == "e") {
+//         vowCount = vowCount + 1
+//         }if (word[i] == "i") {
+//             vowCount = vowCount + 1
+//             }if (word[i] == "o") {
+//                 vowCount = vowCount + 1
+//                 }if (word[i] == "u") {
+//                     vowCount = vowCount + 1
+//                     }
+// }
+
+// console.log(vowCount)
+
+
+
+
+// Reverse a word
+
+let word = "apple"
+
+
+for (let i = 0; i < word.length; i ++) {}
+
+
+
+
+
+
+
+
+
